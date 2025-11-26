9ccf2120382d87ad4945a74d78c200ee265e25a6
Kaushal-Pandey1 <161994470+Kaushal-Pandey1@users.noreply.github.com>
2025-11-27 02:14:05 +0530

    Update BirthdaySite.jsx

--- my-birthday-site/src/BirthdaySite.jsx
+++ my-birthday-site/src/BirthdaySite.jsx
@@ -447,7 +447,7 @@ useEffect(() => {
             <button
               onClick={()=>{
               handleCelebrateClick();
-               <audio src="/birthday.mp3" autoPlay loop />}}
+              }}
               className="mt-6 px-6 py-2 bg-pink-600 text-white rounded-full shadow-lg hover:scale-105 transition"
             >
               Tap to Celebrate!
